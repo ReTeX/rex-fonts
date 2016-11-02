0a4c85f8f3d65eaeb684c7102f73e9b72608384f
cbreeden <github@u.breeden.cc>
2016-11-02 01:06:16 -0500

    update

--- generate_glyph_map.py
+++ generate_glyph_map.py
@@ -9,42 +9,73 @@ if len(sys.argv) != 3:
           " their corresponding bounding boxes, and advance widths")
     sys.exit(1)
 
-# This snippet will list all glyphs that are not reachable from the cmap
 in_font  = sys.argv[1]
 out_font = sys.argv[2]
+font = TTFont(in_font)
+glyphset = font.getGlyphSet()
 
-font   = TTFont(in_font)
+# Find all unique glyphs by unicode
 cmaps  = font['cmap'].tables
-glyphs = font.getGlyphNames()
-
-# Find all unique GlyphNames that is reachable from a cmap
-mapped = []
+glyphs = {}
 for cmap in cmaps:
     for key, value in cmap.cmap.items():
-        if value not in mapped:
-            mapped.append(value)
+        if key not in glyphs:
+            glyphs[key] = value
 
-# This acts to filter out GlyphNames we don't care about
-def valid(glyph):
-    # ssty glyphs -- may be useful later on for script variants
-    if glyph[-2:] == 'st' or glyph[-2:] == 'ts':
-        return False
-    return True
+header="""\
+<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="80ex" height="500ex" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <style type="text/css">
+      @font-face {
+        font-family: rex;
+        src: url('dist/rex-xits.otf');
+      }
+    </style>
+  </defs>
+  <g font-family="rex">
+"""
 
-diff = [ glyph for glyph in glyphs 
-            if glyph not in set(mapped) and valid(glyph) ]
+rect_template  = '<rect x="{}ex" y="{}ex" width="{}ex" height="{}ex" fill="none" stroke="blue" stroke-width="0.2px"/>\n'
+glyph_template = '<text x="{}ex" y="{}ex">{}</text>\n'    
 
-# The CMAPS in XITS is:
-# Format 4  in 0 and 4
-# Format 12 in 1 and 5
-base  = 0xE700
-start = base
-for glyph in diff:
-    #print("Adding {} to code point 0x{:X}".format(glyph, start))
-    for table in cmaps:
-        if table.format == 4 or table.format == 12:
-            table.cmap[start] = glyph
-    start += 1
+# Get the bounding box
+bbox_pen = BoundsPen(None)
+def get_bbox(glyph):
+    glyph.draw(bbox_pen)
+    if bbox_pen.bounds == None:
+        return None
+    (xmin, ymin, xmax, ymax) = bbox_pen.bounds
+    bbox_pen.bounds = None
+    bbox_pen._start = None
+    return (xmin/450, ymin/450, xmax/450, ymax/450)
 
-print("\nFinished.  Added {} glyphs to CMAPs in ranges {:X} to {:X}".format(start - base, base, start))
-font.save(out_font)
+# Draw glyphs
+y  = 3
+lh = 0
+
+
+count = 0
+
+for code, name in glyphs.items():
+    if code < 100: continue
+    if count > 16: break
+    bounds = get_bbox(glyphset.get(name))
+    if bounds == None: continue
+    (xmin, ymin, xmax, ymax) = bounds
+    header += glyph_template.format(
+        5*count + 2.5 - (xmax-xmin)/2, 
+        y, 
+        chr(code))
+    header += rect_template.format(
+        5*count + 2.5 - (xmax-xmin)/2 + xmin, 
+        y - ymax, 
+        xmax-xmin, 
+        ymax-ymin)
+    count += 1
+
+header += "</g></svg>"
+with open(out_font, 'w') as f:
+    f.write(header)
