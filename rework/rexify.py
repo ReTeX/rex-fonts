5401ef1a4d880630c3a801f8f3668bea94ba0ba4
Christopher Breeden <github@u.breeden.cc>
2017-07-06 14:38:23 -0500

    continue refactor, this should be a branch but the code base was already a huge mess

--- rework/rexify.py
+++ rework/rexify.py
@@ -1,67 +1,156 @@
-# The purpose of this script is to place all glyphs that aren't accessible from
-# the various CMAPs to PUA.  Then to modify the CMAPs to make the glyphs accessible.
-#
-# TODO: GlyphIDs should be a continuous region with respect to PUA usv.
-# TODO: Deleted glyphs included here.
+'''
+rexify.py
 
+Prepare an OpenType font for use with ReX.  Rexify will look for glyphs that
+are not accessible from a CMAP, place those glyphs into a Private Usage Area
+and modify the CMAPs to include these glyphs.  This is necessary for when
+you want to render math in SVG by referring to glyphs from their usv.
+'''
+
+import json
 from fontTools.ttLib import TTFont
-from itertools import takewhile
-from operator import itemgetter
+from fontTools.pens.boundsPen import BoundsPen
+
+# TODO: Deleted undesired glyphs.
+# TODO: We need to modify the name/copyright to adhere to SIL license.
+
 
 def rexify(font, out):
-    font = TTFont(font)
-    cmap = set(font['cmap'].buildReversed().keys())
-    glyphs = sorted(font.getGlyphSet().keys())
+    '''
+    Rexify font.
 
-    # All glyph indices that are not accessible via cmap
-    diff = [ glyph for glyph in glyphs if glyph not in cmap ]
+    This will take all inaccessible glyphs from the font,
+    place them in a PUA, and modify the CMAPs so that these
+    glyphs are publicly accessible.
 
-    # First find a suitable region in the PUA, U+E000–U+F8FF
-    # We will do this by calculating the empty regions in codepoints and find the first
-    # region is is large enough to fit the `diff` list.
-    required = len(diff)
-    used_usv = font['cmap'].getcmap(3,1).cmap.keys()
-    unused = ( usv for usv in range(0xE000, 0xF8FF) if usv not in used_usv )
+    This will also generate the metrics for `font` and store
+    them at `out`.metrics.
+    '''
+
+    ttfont = TTFont(font)
+    make_accessible(ttfont)
+    generate_metrics(ttfont, out + ".metrics")
+    ttfont.save(out)
+
+def make_accessible(ttfont):
+    '''
+    This will take all inaccessible glyphs from the font,
+    place them in a PUA, and modify the CMAPs so that these
+    glyphs are publicly accessible.
+    '''
+
+    accessible = set(ttfont['cmap'].buildReversed().keys())
+    glyphs = sorted(ttfont.getGlyphSet().keys())
+
+    unaccessible = sorted(
+        [glyph for glyph in glyphs if glyph not in accessible])
+    required = len(unaccessible)
+
+    if required == 0:
+        print("All glyphs are accessible!")
+        return
+    else:
+        print(required, "glyphs need to be placed into PUA.")
+
+    # Find a suitably long vacant region in the PUA, U+E000–U+F8FF.
+    used = ttfont['cmap'].getcmap(3, 1).cmap.keys()
+    unused = (usv for usv in range(0xE000, 0xF8FF) if usv not in used)
 
     start = next(csv for csv, l in ContinuousRegions(unused) if l >= required)
-    print("Placing glyphs at", hex(start))
+    print("Placing PUA glyphs at", hex(start))
 
-# Helper class.  Takes an iterator of numbers, and returns a
-# tuple of (start, length) for continuous regions.
+    for cmap in ttfont['cmap'].tables:
+        if not cmap.isUnicode():
+            print("Skipping", cmap, "as non-unicode table")
+            continue
+
+        for offset, glyph in enumerate(unaccessible):
+            cmap.cmap[start + offset] = glyph
+
+    # To prevent thrashing the CMAPs, we reorder the glyph indices
+    # to match the order for which they are placed in PUA
+    cache = set(unaccessible)
+    new_order = [g for g in ttfont.getGlyphOrder() if g not in cache] + \
+        unaccessible
+
+    ttfont.setGlyphOrder(new_order)
+
+
+def generate_metrics(ttfont, out):
+    '''
+    Generate the metrics for `font` and store them at `out` in json
+    '''
+
+    print("Generating glyph metrics.")
+    glyph_set = ttfont.getGlyphSet()
+    glyphs = ttfont['cmap'].buildReversed()
+    metrics = {}
+
+    pen = BoundsPen(None)
+    for name, usvs in glyphs.items():
+        metric = (0, 0, 0, 0)
+        glyph_set.get(name).draw(pen)
+
+        if pen.bounds is not None:
+            (xm, ym, xM, yM) = pen.bounds
+            metric = (int(xm), int(ym), int(xM), int(yM))
+
+        for usv in iter(usvs):
+            metrics[int(usv)] = metric
+
+        pen.bounds = None
+        pen._start = None
+
+    with open(out, 'w') as file:
+        json.dump(metrics, file)
+
 class ContinuousRegions:
+    '''
+    Continuous Regions.
+
+    This is an iterator adapter which takes an iterator of numbers
+    and returns tupples (start, length) of indicated contiuos regions
+    starting at `start` of length `length`.
+    '''
+
     def __init__(self, it):
-        self.it = it
+        self._it = it
+        self._peek = next(self._it)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        start = next(self.it)
-        count = 1 + sum(1 for _ in
-            takewhile(lambda t: t[1] - t[0] == start + 1, enumerate(self.it)))
+        start = self._peek
+        count = 1
+        for idx, csv in enumerate(self._it):
+            self._peek = csv
+            if csv - idx == start + 1:
+                count += 1
+            else:
+                break
+
         return (start, count)
 
 if __name__ == "__main__":
-    import os
     import sys
 
-    def usage():
-        print("usage: rexify.py in.otf out.otf")
-        print("`rexify.py` will gather all glyphs from `in.otf` that aren't accessible "
-              "from a CMAP and place them into Private Use Area codepoitns. Then "
-              "`rexify.py` will update the CMAPs to make these glyphs accessible.")
+    USAGE = "usage: rexify.py in.otf out.otf\n" \
+            "`rexify.py` will gather all glyphs from `in.otf` that aren't accessible " \
+            "from a CMAP and place them into Private Use Area codepoitns. Then " \
+            "`rexify.py` will update the CMAPs to make these glyphs accessible."
 
     if len(sys.argv) < 3:
-        usage()
+        print(USAGE)
         sys.exit(2)
 
     if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-        usage()
+        print(USAGE)
         sys.exit(0)
 
-    font = sys.argv[1]
-    out = sys.argv[2]
+    FONT = sys.argv[1]
+    OUT = sys.argv[2]
 
-    print("Rexifying: ", font)
-    rexify(font, out)
-    print("Finished rexifying.")
+    print("Rexifying:", FONT)
+    rexify(FONT, OUT)
+    print("Finished rexifying:", OUT)
